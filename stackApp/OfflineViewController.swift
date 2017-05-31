172ea938e01a1615a82e2c5b7f5edbf9d0540cf7
Keshav Bansal <keshav_x11@yahoo.co.in>
2017-06-01 00:49:43 +0530

    update

--- stackApp/OfflineViewController.swift
+++ stackApp/OfflineViewController.swift
@@ -92,31 +92,6 @@ class OfflineViewController: UIViewController, UITableViewDelegate, UITableViewD
         
     }
     
-    //    func hexStringToUIColor (_ hex:String) -> UIColor {
-    //        var cString:String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
-    //
-    //        if (cString.hasPrefix("#")) {
-    //            let index1 = cString.characters.index(cString.endIndex, offsetBy: -(cString.characters.count-1))
-    //            cString = cString.substring(from: index1)
-    //        }
-    //
-    //        if (cString.characters.count != 6) {
-    //            return UIColor.gray
-    //        }
-    //
-    //        var rgbValue:UInt32 = 0
-    //        Scanner(string: cString).scanHexInt32(&rgbValue)
-    //
-    //        return UIColor(
-    //            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-    //            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-    //            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-    //            alpha: CGFloat(1.0)
-    //        )
-    //    }
-    
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
