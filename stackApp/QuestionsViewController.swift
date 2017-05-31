296e0992d3f289d24f4d16d6d475fcc3ddab4348
Keshav Bansal <keshav_x11@yahoo.co.in>
2017-06-01 00:30:39 +0530

    final commit

--- stackApp/QuestionsViewController.swift
+++ stackApp/QuestionsViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class QuestionsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
+    @IBOutlet var detailView: UIView!
+    @IBOutlet var blurView: UIView!
+    
+    var tap :UITapGestureRecognizer!
     var sortParam = "hot"
     var filterParam = ""
     var tagText = ""
@@ -22,25 +26,65 @@ class QuestionsViewController: UIViewController, UITableViewDelegate, UITableVie
     var quesArray = NSArray()
     @IBOutlet var sortBtn: UIButton!
     @IBOutlet var filterBtn: UIButton!
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var voteLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var ownerLabel: UILabel!
+    @IBOutlet var answeredLabel: UILabel!
+    @IBOutlet var answerCountLabel: UILabel!
+    
+    @IBOutlet var linkBtn: UIButton!
+    @IBOutlet var offlineBtn: UIButton!
+    
+    var currentIndex: Int!
+    
+    @IBAction func visitLink(){
+        
+        let dict = self.quesArray.object(at: currentIndex) as! NSDictionary
+        let link = dict.object(forKey: "link") as! String
+        UIApplication.shared.open(URL(string:link)!, options: [:], completionHandler: nil)
+        
+    }
+    
+    @IBAction func addtoOffline(){
+        
+        let dict = self.quesArray.object(at: currentIndex) as! NSDictionary
+        let array = NSMutableArray()
+        if UserDefaults.standard.value(forKey: "offline") != nil{
+            let arr = (UserDefaults.standard.object(forKey: "offline") as! NSArray).mutableCopy()
+            array.addObjects(from: arr as! [AnyObject])
+        }
+        array.add(dict)
+        UserDefaults.standard.set(array as NSMutableArray, forKey: "offline")
+        UserDefaults.standard.synchronize()
+    }
 
+
+    func setLoad(){
+        self.tableView.isHidden = true
+        self.activityIndicator.isHidden = false
+        self.activityIndicator.startAnimating()
+        self.activityIndicator.hidesWhenStopped = true
+    }
+    
+    func endLoad(){
+        self.activityIndicator.stopAnimating()
+        self.tableView.isHidden = false
+    }
+    
     @IBAction func sort(){
         
         let alertController = UIAlertController(title: "Select an option", message: nil, preferredStyle: .actionSheet)
         
         alertController.addAction(UIAlertAction(title: "Creation", style: .default, handler: {(action:UIAlertAction) in
-            self.tableView.isHidden = true
-            self.activityIndicator.isHidden = false
-            self.activityIndicator.startAnimating()
-            self.activityIndicator.hidesWhenStopped = true
+            self.setLoad()
             self.sortParam = "creation"
             self.getJson()
         }))
         
         alertController.addAction(UIAlertAction(title: "Votes", style: .default, handler: {(action:UIAlertAction) in
-            self.tableView.isHidden = true
-            self.activityIndicator.isHidden = false
-            self.activityIndicator.startAnimating()
-            self.activityIndicator.hidesWhenStopped = true
+            self.setLoad()
             self.sortParam = "votes"
             self.getJson()
         }))
@@ -57,10 +101,7 @@ class QuestionsViewController: UIViewController, UITableViewDelegate, UITableVie
         let alertController = UIAlertController(title: "Select an option", message: nil, preferredStyle: .actionSheet)
         
         alertController.addAction(UIAlertAction(title: "Unanswered", style: .default, handler: {(action:UIAlertAction) in
-            self.tableView.isHidden = true
-            self.activityIndicator.isHidden = false
-            self.activityIndicator.startAnimating()
-            self.activityIndicator.hidesWhenStopped = true
+            self.setLoad()
             self.filterParam = "/unanswered"
             if self.sortParam == "hot"{
                 self.sortParam = "votes"
@@ -69,10 +110,7 @@ class QuestionsViewController: UIViewController, UITableViewDelegate, UITableVie
         }))
         
         alertController.addAction(UIAlertAction(title: "Featured", style: .default, handler: {(action:UIAlertAction) in
-            self.tableView.isHidden = true
-            self.activityIndicator.isHidden = false
-            self.activityIndicator.startAnimating()
-            self.activityIndicator.hidesWhenStopped = true
+            self.setLoad()
             self.filterParam = "/featured"
             if self.sortParam == "hot"{
                 self.sortParam = "votes"
@@ -105,9 +143,12 @@ class QuestionsViewController: UIViewController, UITableViewDelegate, UITableVie
                     let dict = parsedData as! Dictionary<String, Any>
                     
                     self.quesArray = dict["items"] as! NSArray
+                    var array = NSArray()
+                    array = self.quesArray
+                    UserDefaults.standard.set(array, forKey: "lastFetch")
+                    UserDefaults.standard.synchronize()
                     self.tableView.reloadData()
-                    self.activityIndicator.stopAnimating()
-                    self.tableView.isHidden = false
+                    self.endLoad()
                     
                 } catch let error as NSError {
                     print(error)
@@ -119,6 +160,8 @@ class QuestionsViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         searchActive = true;
+        tap = UITapGestureRecognizer(target: self, action:#selector(QuestionsViewController.tapped))
+        self.view.addGestureRecognizer(tap)
     }
     
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
@@ -133,38 +176,61 @@ class QuestionsViewController: UIViewController, UITableViewDelegate, UITableVie
         searchActive = false
         searchBar.resignFirstResponder()
         if searchBar.text != nil{
-            self.tableView.isHidden = true
-            self.activityIndicator.isHidden = false
-            self.activityIndicator.startAnimating()
+            self.setLoad()
             tagText = String(describing: searchBar.text!)
-            getJson()
+            self.getJson()
         }
     }
     
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        
-//        filtered = data.filter({ (text) -> Bool in
-//            let tmp: NSString = text
-//            let range = tmp.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
-//            return range.location != NSNotFound
-//        })
-//        if(filtered.count == 0){
-//            searchActive = false;
-//        } else {
-//            searchActive = true;
-//        }
-//        self.tableView.reloadData()
-//    }
-
+    
+    func tapped(){
+        
+        searchBar.resignFirstResponder()
+        view.removeGestureRecognizer(tap)
+        
+    }
+    
+    @IBAction func tapDetailExit(){
+        
+        self.detailView.frame.origin.y = self.view.frame.midX - self.detailView.frame.width/2
+        self.detailView.frame.origin.y = self.view.frame.height
+        self.blurView.isHidden = true
+        self.detailView.isHidden = true
+        blurView.removeGestureRecognizer(tap)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        
+        linkBtn.layer.cornerRadius = 5
+        linkBtn.layer.masksToBounds = true
+        offlineBtn.layer.cornerRadius = 5
+        offlineBtn.layer.masksToBounds = true
+        self.detailView.layer.cornerRadius = 7
+        self.detailView.layer.masksToBounds = true
+        self.detailView.frame.origin.x = self.view.frame.midX - self.detailView.frame.width/2
+        self.detailView.frame.origin.y = self.view.frame.height
+        detailView.isHidden = false
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         sortBtn.layer.cornerRadius = 5
         sortBtn.layer.masksToBounds = true
         filterBtn.layer.cornerRadius = 5
         filterBtn.layer.masksToBounds = true
         self.activityIndicator.startAnimating()
         self.activityIndicator.hidesWhenStopped = true
-        self.getJson()
+        
+        if UserDefaults.standard.value(forKey: "lastFetch") == nil{
+            self.getJson()
+        }else{
+            self.quesArray = UserDefaults.standard.object(forKey: "lastFetch") as! NSArray
+            self.endLoad()
+        }
 
     }
     
@@ -212,34 +278,46 @@ class QuestionsViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        currentIndex = indexPath.row
         let dict = self.quesArray.object(at: indexPath.row) as! NSDictionary
-        let link = dict.object(forKey: "link") as! String
+        let title = dict.object(forKey: "title") as! String
+        let votes = dict.object(forKey: "score") as! NSNumber
+        let answers = dict.object(forKey: "answer_count") as! NSNumber
+        let onwerName = (dict.object(forKey: "owner") as! NSDictionary).object(forKey: "display_name") as! String
+        if dict.object(forKey: "is_answered") as! Bool == false{
+            self.answeredLabel.textColor = UIColor.red
+            self.answeredLabel.text = "Unanswered"
+        }
+            
+        let timeResult = dict.object(forKey: "creation_date") as! Double
+        let date = NSDate(timeIntervalSince1970: timeResult)
+        let dayTimePeriodFormatter = DateFormatter()
+        dayTimePeriodFormatter.dateFormat = "dd MMM YYYY"
+        let dateString = dayTimePeriodFormatter.string(from: date as Date)
+            
+        self.titleLabel.text = title
+        self.ownerLabel.text = "Posted by: " + onwerName
+        self.voteLabel.text = String(describing: votes)
+        self.answerCountLabel.text = "Answers: " + String(describing: answers)
+        self.dateLabel.text = "Posted: " + String(describing: dateString)
+
+        self.blurView.isHidden = false
         
-        let alertController = UIAlertController(title: "Select an option", message: nil, preferredStyle: .actionSheet)
+        tap = UITapGestureRecognizer(target: self, action:#selector(QuestionsViewController.tapDetailExit))
+        self.blurView.addGestureRecognizer(tap)
         
-        alertController.addAction(UIAlertAction(title: "Add to Offline", style: .default, handler: {(action:UIAlertAction) in
-            let array = NSMutableArray()
-            if UserDefaults.standard.value(forKey: "offline") != nil{
-                let arr = (UserDefaults.standard.object(forKey: "offline") as! NSArray).mutableCopy()
-                array.addObjects(from: arr as! [AnyObject])
-            }
-            array.add(dict)
-            UserDefaults.standard.set(array as NSMutableArray, forKey: "offline")
-            UserDefaults.standard.synchronize()
-        }))
+        UIView.animate(withDuration: 1.0, animations: {
+            
+            self.detailView.frame.origin.y = self.view.frame.midY - self.detailView.frame.height/2
+            
+        })
         
-        alertController.addAction(UIAlertAction(title: "Visit Question link", style: .default, handler: {(action:UIAlertAction) in
-            UIApplication.shared.open(URL(string:link)!, options: [:], completionHandler: nil)
-        }))
-        
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {(action:UIAlertAction) in
-            alertController.dismiss(animated: true, completion: nil)
-        }))
-        
-        self.present(alertController, animated: true, completion: nil)
+        detailView.isHidden = false
         self.tableView.deselectRow(at: indexPath, animated: true)
         
     }
+    
 
     //    func hexStringToUIColor (_ hex:String) -> UIColor {
     //        var cString:String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
